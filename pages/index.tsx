2dba5224cf4e00554052ede5b48acd66b3e31d24
Fortan Pireva <fortanpireva99@gmail.com>
2023-10-09 12:27:05 +0200

    swap pages for kreativat

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,129 +1,31 @@
-import Link from "next/link";
+import React from "react";
+import Footer from "../components/Footer/Footer";
+import Header from "../components/Header/Header";
+import Hero from "../components/Hero/Hero";
 import Layout from "../components/Layout";
-import { useState } from "react";
-import ReactLoading from "react-loading";
-import { useFormik } from "formik";
-import * as yup from "yup";
-interface FormikInitialValues {
-  email: string;
-}
+import Main from "../containers/Main/Main";
+import { footerLinks, links, socialMediaLinks } from "../data/data";
 
-const validationSchema = yup.object({
-  email: yup.string().email().required(),
-});
-
-const IndexPage = () => {
-  const [errorMsg, setErrorMsg] = useState("");
-  const [isSendingMail, setIsSendingMail] = useState(false);
-  const initialValues: FormikInitialValues = { email: "" };
-
-  const formik = useFormik({
-    initialValues: initialValues,
-    validationSchema: validationSchema,
-    onSubmit: async (values) => {
-      try {
-        setIsSendingMail(true);
-        const res = await fetch("/api/newsletter", {
-          method: "POST",
-          body: values.email,
-        });
-
-        const data = await res.json();
-
-        setIsSendingMail(false);
-        if (data.error !== null) {
-          throw data.error;
-        }
-        setErrorMsg(null);
-      } catch (e) {
-        setErrorMsg(e);
-      } finally {
-        setIsSendingMail(false);
-        setTimeout(() => {
-          setErrorMsg("");
-        }, 5000);
-      }
-    },
-  });
-
+const NewDesignPage = () => {
   return (
     <Layout
-      title="Kreativat -  veprat krijuese të të rinjëve kosovar"
+      title="Kreativat - veprat krijuese të të rinjëve kosovar"
       description="Kreativat - platformë për shpalosjen e talentit kosovar, në fushat e teknologjisë, ndërmarrësisë, inovacionit."
     >
-      <div className="bg-black text-white font-bebas-neue bg-no-repeat w-full  mx:auto bg-cover  min-h-screen h-screen  relative z-10 py-6 space-y-16 lg:space-y-24">
-        <div className="text-center w-full mr-0 pt-28 flex justify-center items-center flex-col">
-          {/* <TextAnimtation /> */}
-          {/* <h1 className="text-7xl lg:text-9xl font-extrabold">We’r blowing up</h1> */}
-          <p className="text-xl text-kreativat-faint-white font-unica-one lg:text-lg tracking-wide mx-10 lg:max-w-3xl lg:mx-auto uppercase">
-            Së shpejti...
-          </p>
-          <h1 className="text-8xl lg:text-9xl uppercase">KREATIVAT.</h1>
-          <p className="text-xl md:text-2xl max-w-xl font-unica-one  text-kreativat-faint-white tracking-wide mx-10 lg:max-w-md lg:mx-auto uppercase">
-            Platformë për promovimin e kreativitetit e inovacionit të të rinjve
-            shqiptarë.
-          </p>
-        </div>
-        <div className="relative z-10 mx-3 flex flex-col justify-center items-center">
-          <form
-            className=" flex-1 gap-0 lowercase  w-full max-w-xl relative  mx-4"
-            onSubmit={formik.handleSubmit}
-          >
-            <input
-              type="email"
-              id="email"
-              name="email"
-              onChange={formik.handleChange}
-              value={formik.values.email}
-              placeholder="E-maili juaj"
-              className={` w-full flex-1  bg-white text-2xl outline-none  lowercase font-unica-one text-black placeholder-kreativat-input-placeholder py-6 pl-5 pr-32 md:pr-42 rounded-sm
-                ${
-                  Boolean(formik.errors.email) && formik.values.email.length > 0
-                    ? "border-red-500 border-2"
-                    : ""
-                }
-              `}
-            />
-
-            <button
-              type="submit"
-              className="absolute uppercase right-2  top-3  w-28 py-4 font-unica-one    md:w-40    md:px-5 md:py-4 text-xl font-semibold bg-kreativat-input-black text-white  transition ease-in-out duration-500 hover:scale-105"
-            >
-              {isSendingMail ? (
-                <div>
-                  <ReactLoading
-                    type={"spin"}
-                    color={"#fff"}
-                    width={30}
-                    height={30}
-                  />
-                </div>
-              ) : (
-                <>Më njofto </>
-              )}
-            </button>
-          </form>
-
-          {Boolean(formik.errors.email) && formik.values.email.length > 0 && (
-            <p className="text-red-500 mt-3 font-unica-one text-lg pl-2">
-              {formik.errors.email}
-            </p>
-          )}
-          {errorMsg && (
-            <p className="text-red-500 text-center mt-3 font-unica-one text-lg pl-2 ">
-              Oops... ka ndodhur një gabim.
-            </p>
-          )}
-          {errorMsg === null && (
-            <p className="text-green-500 text-center  mt-3">
-              Ju faleminderit për abonimin. Do ju njoftojmë kur të lansohet
-              platforma
-            </p>
-          )}
-        </div>
+      <div className="bg-hero bg-no-repeat w-full bg-cover  min-h-screen  h-screen flex flex-col max-w-screen-xl overflow-hidden">
+        <Header />
+        <Hero />
       </div>
+      <Main />
+      <Footer
+        company="Kreativat"
+        year={new Date().getFullYear()}
+        links={links}
+        socialMediaLinks={socialMediaLinks}
+        externalLinks={footerLinks}
+      />
     </Layout>
   );
 };
 
-export default IndexPage;
+export default NewDesignPage;
